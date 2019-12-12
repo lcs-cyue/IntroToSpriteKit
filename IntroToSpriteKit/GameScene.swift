739971d80d88cb9def3e32198d6cbc64685e2f1b
Chelsea Yue <cyue@lcs.on.ca>
2019-12-11 21:26:32 -0500

    Added particles

--- IntroToSpriteKit/GameScene.swift
+++ IntroToSpriteKit/GameScene.swift
@@ -11,7 +11,8 @@ import GameplayKit
 import AVFoundation
 
 class GameScene: SKScene {
-  
+    
+    
     // Background music player
     var backgroundMusic: AVAudioPlayer?
     
@@ -24,15 +25,25 @@ class GameScene: SKScene {
         bg.position = CGPoint(x: 400, y: 300)
         self.addChild(bg) 
         
+        //Add a hat for WALL-E
         let hat = SKSpriteNode(imageNamed: "hat")
         hat.position = CGPoint(x: 570, y: 390)
         self.addChild(hat)
-       
-    
         
-
-//        // Set the background colour
-//        self.backgroundColor = .black
+        //Add a falling gift box
+        
+        //Add snow
+        if let snow = SKEmitterNode(fileNamed: "snow.sks") {
+            snow.position = CGPoint(x: 400, y: 600)
+            self.addChild(snow)
+        }
+        
+        //Add fireflies
+        if let fireflies = SKEmitterNode(fileNamed: "fireflies.sks") {
+            fireflies.position = CGPoint(x: 400, y: -250)
+            self.addChild(fireflies)
+        }
+        
         
         // Get a reference to the mp3 file in the app bundle
         let backgroundMusicFilePath = Bundle.main.path(forResource: "sleigh-bells-excerpt.mp3", ofType: nil)!
@@ -47,7 +58,7 @@ class GameScene: SKScene {
         } catch {
             // Do nothing if the sound file could not be played
         }
-
+        
     }
     
     // This runs before each frame is rendered
